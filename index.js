33d4b31ecf56373dead3e10086e32592bab3291c
Joe Smith <joeycrushed@gmail.com>
2021-10-13 17:16:28 +0100

    dynamnic url

--- index.js
+++ index.js
@@ -1,52 +1,51 @@
 const PORT = 8000
 
 const express = require('express')
+const app = express()
 const cheerio = require('cheerio')
 const axios = require('axios')
 const fs = require('fs')
-const writeStream = fs.createWriteStream
 
-const app = express()
-const url = "https://www.pro-football-reference.com/years/2021/week_5.htm"
-
-axios(url)
-.then(response => {
-    const html = response.data
-    const $ = cheerio.load(html)
-    const nflWeekData = []
-    
-    $('div.section_heading', html).each(function() {
-        const week = $(this).find('h2:nth-child(1)').text()
-        if(week != "" && week != null) {
-            nflWeekData.push(week)
-        }
-    })
-
-    $('table.teams', html).each(function() {
-        const date = $(this).find('tr.date').text()
-        const winningTeam = $(this).find('tr.winner > td:nth-child(1)').text()
-        const winningTeamScore = $(this).find('tr.winner > td:nth-child(2)').text()
-        const winningTeamStatPage = `https://www.pro-football-reference.com${$(this).find('a').attr('href')}`
-        const losingTeam = $(this).find('tr.loser > td:nth-child(1)').text()
-        const losingTeamScore = $(this).find('tr.loser > td:nth-child(2)').text()
-        const losingTeamStatPage = `https://www.pro-football-reference.com${$(this).find('tr.loser > td > a').attr('href')}`
-        const gameLink = `https://www.pro-football-reference.com${$(this).find('td.gamelink > a').attr('href')}`
+const getNFLData = (week, year) => {
+    axios(`https://www.pro-football-reference.com/years/${year}/week_${week}.htm`)
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const nflWeekData = []
         
-        nflWeekData.push({
-            date,
-            winningTeam,
-            winningTeamScore,
-            losingTeam,
-            losingTeamScore,
-            winningTeamStatPage,
-            losingTeamStatPage,
-            gameLink
+        $('div.section_heading', html).each(function() {
+            const week = $(this).find('h2:nth-child(1)').text()
+            if(week != "" && week != null) {
+                nflWeekData.push(week)
+            }
         })
-    })
-    console.log(nflWeekData)
-}).catch(err => console.log(err))
 
+        $('table.teams', html).each(function() {
+            const date = $(this).find('tr.date').text()
+            const winningTeam = $(this).find('tr.winner > td:nth-child(1)').text()
+            const winningTeamScore = $(this).find('tr.winner > td:nth-child(2)').text()
+            const winningTeamStatPage = `https://www.pro-football-reference.com${$(this).find('a').attr('href')}`
+            const losingTeam = $(this).find('tr.loser > td:nth-child(1)').text()
+            const losingTeamScore = $(this).find('tr.loser > td:nth-child(2)').text()
+            const losingTeamStatPage = `https://www.pro-football-reference.com${$(this).find('tr.loser > td > a').attr('href')}`
+            const gameLink = `https://www.pro-football-reference.com${$(this).find('td.gamelink > a').attr('href')}`
+            
+            nflWeekData.push({
+                date,
+                winningTeam,
+                winningTeamScore,
+                losingTeam,
+                losingTeamScore,
+                winningTeamStatPage,
+                losingTeamStatPage,
+                gameLink
+            })
+        })
+        console.log(nflWeekData)
+    }).catch(err => console.log(err))
+}
 
+getNFLData(5, 2021)
 
 app.listen(PORT, () => 
     console.log(`server running on PORT ${PORT}`)
