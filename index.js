65b5754a8b2b00cb5b3e3d21cc46acb2862d44c4
Joe Smith <joeycrushed@gmail.com>
2021-10-13 17:11:57 +0100

    pro-football data

--- index.js
+++ index.js
@@ -3,33 +3,50 @@ const PORT = 8000
 const express = require('express')
 const cheerio = require('cheerio')
 const axios = require('axios')
-
+const fs = require('fs')
+const writeStream = fs.createWriteStream
 
 const app = express()
-const url = "https://thescore.com/nfl/events/date/2021-5"
-
+const url = "https://www.pro-football-reference.com/years/2021/week_5.htm"
 
 axios(url)
 .then(response => {
     const html = response.data
     const $ = cheerio.load(html)
-    const lost = []
-    const won = []
-    const scores = []
-    const data = []
-    $('.EventCard__eventCardContainer--3hTGN', html).each(function() {
-        const lostTeam = $(this).find('div.EventCard__teamLost--2dtcV:nth-child(2)').text()
-        const winningTeam = $(this).find('div.EventCard__topRow--oL3iY > div.EventCard__teamColumn--17asJ:nth-child(1)').text()
-        const score = $(this).find('div.EventCard__scoreColumn--2JZbq').text()
-        const matchScore = score.substring(0,2) + ' - ' + score.substring(2,6)
-        data.push({
+    const nflWeekData = []
+    
+    $('div.section_heading', html).each(function() {
+        const week = $(this).find('h2:nth-child(1)').text()
+        if(week != "" && week != null) {
+            nflWeekData.push(week)
+        }
+    })
+
+    $('table.teams', html).each(function() {
+        const date = $(this).find('tr.date').text()
+        const winningTeam = $(this).find('tr.winner > td:nth-child(1)').text()
+        const winningTeamScore = $(this).find('tr.winner > td:nth-child(2)').text()
+        const winningTeamStatPage = `https://www.pro-football-reference.com${$(this).find('a').attr('href')}`
+        const losingTeam = $(this).find('tr.loser > td:nth-child(1)').text()
+        const losingTeamScore = $(this).find('tr.loser > td:nth-child(2)').text()
+        const losingTeamStatPage = `https://www.pro-football-reference.com${$(this).find('tr.loser > td > a').attr('href')}`
+        const gameLink = `https://www.pro-football-reference.com${$(this).find('td.gamelink > a').attr('href')}`
+        
+        nflWeekData.push({
+            date,
             winningTeam,
-            lostTeam,
-            matchScore
+            winningTeamScore,
+            losingTeam,
+            losingTeamScore,
+            winningTeamStatPage,
+            losingTeamStatPage,
+            gameLink
         })
     })
-    console.log(data)
+    console.log(nflWeekData)
 }).catch(err => console.log(err))
+
+
 
 app.listen(PORT, () => 
     console.log(`server running on PORT ${PORT}`)
